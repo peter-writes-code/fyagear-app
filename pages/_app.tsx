f02bb4b851cfdf213c88321712a252ff4a3dad15
peter-writes-code <peter@docpack.com>
2021-04-03 13:16:09 -0700

    removes redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,9 @@ import "tailwindcss/tailwind.css";
 
 import {AppProps} from 'next/app';
 import React, {FC} from 'react';
-import {wrapper} from '../redux/store';
 
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
+const FyaGearApp: FC<AppProps> = ({Component, pageProps}) => (
     <Component {...pageProps} />
 );
 
-export default wrapper.withRedux(WrappedApp);
+export default FyaGearApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
-import { END } from "redux-saga";
 import Link from "next/link";
-import { loadData, startClock, tickClock } from "../redux/actions";
 import React from "react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { wrapper } from "../redux/store";
 
 import AppHead from "../components/common/AppHead";
 
 const Index = () => {
   const title = "fyagear | home";
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    // dispatch(startClock());
-  }, [dispatch]);
 
   return (
     <div className="flex flex-col h-screen bg-gradient-to-br from-gray-200 to-white select-none">
@@ -34,16 +25,5 @@ const Index = () => {
     </div>
   );
 };
-
-export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
-  store.dispatch(tickClock(false));
-
-  if (!store.getState().placeholderData) {
-    store.dispatch(loadData());
-    store.dispatch(END);
-  }
-
-  await (store as any).sagaTask.toPromise();
-});
 
 export default Index;
